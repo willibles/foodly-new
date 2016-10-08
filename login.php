cdc1b44419f27004e5d6545e1789ddb70e7ccb5a
PHPman <dmitrikonnikov@gmail.com>
2016-10-08 14:51:16 -0400

    dashboard login finished

---
+++ login.php
@@ -0,0 +1,10 @@
+<?php
+include('header.php');
+?>
+<body>
+  <form action="loginprocessor.php" method="post">
+    Username: <input type="text" name="uname">
+    Password: <input type="text" name="pword">
+    <button type="submit" name="submit">Log In</button>
+    </form>
+</body>
--- loginprocessor.php
+++ loginprocessor.php
@@ -1,13 +1,18 @@
 <?php
+  require_once('connect.php');
   if(isset($_POST['submit'])){
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-    $result = $db->query("SELECT username,password FROM users WHERE username = '$username' AND password = '$password'");
+    $username = $_POST['uname'];
+    $password = $_POST['pword'];
+    $result = $db->query("SELECT username,password FROM user WHERE username = '$username' AND password = '$password'");
     if(!$result){
       die($db->error);
     }else{
-      setcookie('loggedin','1',time()+604800);
-      header('Location: dashboard.php');
+      if($result->num_rows == 1){
+        session_start();
+        header('Location: dashboard.php');
+      }else{
+        die('Login Failed');
+      }
     }
   }
  ?>
