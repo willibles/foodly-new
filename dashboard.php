806e374f252a02434f95e68c532909acd1bf5c26
PHPman <dmitrikonnikov@gmail.com>
2016-10-08 16:24:01 -0400

    Made it all purty
    (By Willibles using Jack's account)

---
+++ dashboard.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Food.ly</title>
+		<link type="text/css" rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/css/materialize.css"  media="screen,projection"/>
+		<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
+		<link href="https://fonts.googleapis.com/icon?family=Roboto" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/icon?family=Lobster" rel="stylesheet" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+	</head>
+  <style>
+    body{
+      font-family: 'Roboto';
+    }
+    a{font-family: 'Lobster'}
+    li{line-height:25px;}
+  </style>
+	<body>
+		<div class="navbar-fixed">
+			<nav>
+    			<div class="nav-wrapper red accent-3">
+					<a href="#!" class="brand-logo center">Food.ly</a>
+    			</div>
+  			</nav>
+      </div>
+        <div class="container">
+          <div class="row">
+            <div class="col s12">
+              <div class="card">
+                <div class="card-image">
+                  <img src="img/food_img_demo.jpg">
+                  <span class="card-title">Coleslaw Chicken Burger</span>
+                </div>
+                <div class="card-content">
+                  <h3>Ingredients:</h3>
+                  <ol>
+                      <li>Chicken</li>
+                      <li>Coleslaw</li>
+                      <li>Buns</li>
+                  </ol>
+                </div>
+                <div class="card-action">
+                  <a href="#">Recipe</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+  </body>
+<html>
--- login.php
+++ login.php
@@ -1,10 +1,38 @@
 <?php
 include('header.php');
 ?>
-<body>
-  <form action="loginprocessor.php" method="post">
-    Username: <input type="text" name="uname">
-    Password: <input type="text" name="pword">
-    <button type="submit" name="submit">Log In</button>
-    </form>
-</body>
+<html>
+	<head>
+		<title>Food.ly</title>
+		<link type="text/css" rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/css/materialize.css"  media="screen,projection"/>
+		<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
+		<link href="https://fonts.googleapis.com/icon?family=Roboto" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/icon?family=Lobster" rel="stylesheet" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+	</head>
+  <style>
+    body{
+      font-family: 'Roboto';
+    }
+    a{font-family: 'Lobster'}
+  </style>
+	<body>
+		<div class="navbar-fixed">
+			<nav>
+    			<div class="nav-wrapper red accent-3">
+					<a href="#!" class="brand-logo center">Food.ly</a>
+    			</div>
+  			</nav>
+		</div>
+		<div class="container">
+      <div class="row">
+        <div class="col s12 m12">
+          <div class="card white">
+            <div class="card-content" style="margin-top:25vh;">
+                <form action="loginprocessor.php" method="post">
+                  Username: <input type="text" name="uname">
+                  Password: <input type="password" name="pword">
+                  <a href="#" class="waves-effect waves-light btn red"><button style="background:transparent; outline:none; border:none" type="submit" name="submit">Log In</button></a>
+                </form>
+              </body>
+</html>
